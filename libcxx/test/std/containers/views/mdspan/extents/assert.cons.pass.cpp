52914858c98e152943b9af7107bcffcaa8851d32
Christian Trott <crtrott@sandia.gov>
2023-04-12 16:50:07 -0600

    mdspan/extents: add assertion test for mismatched runtime/static extent

---
+++ libcxx/test/std/containers/views/mdspan/extents/assert.cons.pass.cpp
@@ -0,0 +1,59 @@
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+// XFAIL: availability-verbose_abort-missing
+// ADDITIONAL_COMPILE_FLAGS: -D_LIBCPP_ENABLE_ASSERTIONS=1
+
+// <mdspan>
+
+// template<class... OtherIndexTypes>
+//     constexpr explicit extents(OtherIndexTypes...) noexcept;
+//
+// Remarks: These constructors shall not participate in overload resolution unless:
+//   - (is_convertible_v<OtherIndexTypes, index_type> && ...) is true,
+//   - (is_nothrow_constructible_v<index_type, OtherIndexTypes> && ...) is true, and
+//   - N == rank_dynamic() || N == rank() is true.
+//
+//
+// template<class OtherIndexType, size_t N>
+//     constexpr explicit(N != rank_dynamic()) extents(span<OtherIndexType, N>) noexcept;
+// template<class OtherIndexType, size_t N>
+//     constexpr explicit(N != rank_dynamic()) extents(const array<OtherIndexType, N>&) noexcept;
+//
+// Remarks: These constructors shall not participate in overload resolution unless:
+//   - is_convertible_v<const OtherIndexType&, index_type> is true,
+//   - is_nothrow_constructible_v<index_type, const OtherIndexType&> is true, and
+//   - N == rank_dynamic() || N == rank() is true.
+//
+
+#include <mdspan>
+#include <cassert>
+
+#include "check_assertion.h"
+
+int main() {
+  constexpr size_t D = std::dynamic_extent;
+  {
+    std::extents<int, D, 5> e1(3, 5); // should work
+    (void)e1;
+  }
+
+  TEST_LIBCPP_ASSERT_FAILURE(([] { std::extents<int, D, 5> e1(3, 3); }()),
+                             "extents construction: mismatch of provided arguments with static extents.");
+  TEST_LIBCPP_ASSERT_FAILURE(
+      ([] {
+        std::extents<int, D, 5> e1(std::array{3, 3});
+      }()),
+      "extents construction: mismatch of provided arguments with static extents.");
+  TEST_LIBCPP_ASSERT_FAILURE(
+      ([] {
+        std::array a{3, 3};
+        std::span s(a);
+        std::extents<int, D, 5> e1(s);
+      }()),
+      "extents construction: mismatch of provided arguments with static extents.");
+}
