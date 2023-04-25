5f0a60dc4a6ebca1535727f8ad5128025734d471
Christian Trott <crtrott@sandia.gov>
2023-04-25 15:29:09 -0600

    mdspan/extents: split constructor test

---
+++ libcxx/test/std/containers/views/mdspan/extents/ctor_from_integral.pass.cpp
@@ -0,0 +1,74 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// <mdspan>
+
+// constexpr extents() noexcept;
+//
+//
+// template<class... OtherIndexTypes>
+//     constexpr explicit extents(OtherIndexTypes...) noexcept;
+//
+// Remarks: These constructors shall not participate in overload resolution unless:
+//   - (is_convertible_v<OtherIndexTypes, index_type> && ...) is true,
+//   - (is_nothrow_constructible_v<index_type, OtherIndexTypes> && ...) is true, and
+//   - N == rank_dynamic() || N == rank() is true.
+//
+//
+// template<class OtherIndexType, size_t N>
+//     constexpr explicit(N != rank_dynamic()) extents(span<OtherIndexType, N>) noexcept;
+// template<class OtherIndexType, size_t N>
+//     constexpr explicit(N != rank_dynamic()) extents(const array<OtherIndexType, N>&) noexcept;
+//
+// Remarks: These constructors shall not participate in overload resolution unless:
+//   - is_convertible_v<const OtherIndexType&, index_type> is true,
+//   - is_nothrow_constructible_v<index_type, const OtherIndexType&> is true, and
+//   - N == rank_dynamic() || N == rank() is true.
+//
+
+#include <mdspan>
+#include <cassert>
+#include <array>
+#include <span>
+
+#include "ConvertibleToIntegral.h"
+#include "CtorTestCombinations.h"
+#include "test_macros.h"
+
+// std::extents can be constructed from just indices, a std::array, or a std::span
+// In each of those cases one can either provide all extents, or just the dynamic ones
+// If constructed from std::span, the span needs to have a static extent
+// Furthermore, the indices/array/span can have integer types other than index_type
+
+struct IntegralCtorTest {
+  template <class E, class AllExtents, class Extents, size_t... Indices>
+  static constexpr bool test_construction(AllExtents all_ext, Extents ext, std::index_sequence<Indices...>) {
+    // default construction
+    ASSERT_NOEXCEPT(E{});
+    // construction from indices
+    ASSERT_NOEXCEPT(E(ext[Indices]...));
+    return test_runtime_observers(E(ext[Indices]...), all_ext);
+  }
+  /*
+  // construction from array
+  ASSERT_NOEXCEPT(E(ext));
+  test_runtime_observers(E(ext), all_ext);
+  if(constexpr 
+  test_implicit_construction<E>(all_ext, dyn_ext);
+  // construction from span
+  ASSERT_NOEXCEPT(E(std::span(ext)));
+  test_runtime_observers(E(std::span(ext)), all_ext);
+  test_implicit_construction<E>(all_ext, dyn_ext);
+  */
+};
+
+int main() {
+  test_index_type_combo<IntegralCtorTest>();
+  static_assert(test_index_type_combo<IntegralCtorTest>());
+}
